a9ac2f505ab143f392b38c96dba06abe7f22bc4f
jhairzp27 <jhairzp27@hotmail.com>
2026-04-24 17:44:21 -0500

    feat: implement SmartWallet class without unwanted files

--- PracticaPruebasUnitarias/src/main/java/epn/verif/SmartWallet.java
+++ PracticaPruebasUnitarias/src/main/java/epn/verif/SmartWallet.java
@@ -1,4 +1,36 @@
 package epn.verif;
 
 public class SmartWallet {
+    private double balance;
+    private String userType;
+    private String status;
+
+
+    public SmartWallet(double balance, String userType) {
+        this.balance = balance;
+        this.userType = userType;
+        this.status = "Activa";
+    }
+
+    public boolean deposit(double amount){
+        //TODO: Implementar lógica de depósito
+        return false;
+    }
+
+    public boolean withdraw(double amount){
+        //TODO: Implementar lógica de retiro
+        return false;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
